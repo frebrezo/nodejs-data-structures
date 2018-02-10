c5587910916e004ac3f291cc833a33575fc84676
kimwon <kimwon@iit.edu>
2018-02-10 13:06:18 -0500

    Added bubble sort for ArrayList.

---
+++ List/array-list-sort.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var arrayList = require('./array-list');
+
+function bubbleSort(list) {
+    var iLast = list.count - 1;
+    for (var i = 0; i < iLast; ++i) {
+        var swapOccurred = false;
+        var jLast = list.count - 1 - i;
+        for (var j = 0; j < jLast; ++j) {
+            var leftValue = list.getAt(j);
+            var rightValue = list.getAt(j+1);
+            if (leftValue > rightValue) {
+                list.replaceAt(j+1, leftValue);
+                list.replaceAt(j, rightValue);
+                swapOccurred = true;
+            }
+        }
+        if (!swapOccurred) {
+            break;
+        }
+    }
+}
+
+module.exports.bubbleSort = bubbleSort;
---
+++ List/array-list-sort.spec.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var arrayList = require('./array-list');
+var arrayListSort = require('./array-list-sort');
+
+describe('array-list-sort', function() {
+    it('sort', function() {
+        var list = new arrayList.ArrayList();
+        list.add(8);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(1);
+        list.add(0);
+        list.add(9);
+        list.add(7);
+        list.add(2);
+        console.log(list.toString());
+        arrayListSort.bubbleSort(list);
+        console.log(list.toString());
+        for (var i = 0; i < list.count - 1; ++i) {
+            expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
+        }
+    });
+
+    it('sort with duplicates', function() {
+        var list = new arrayList.ArrayList();
+        list.add(8);
+        list.add(1);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(1);
+        list.add(0);
+        list.add(9);
+        list.add(7);
+        list.add(1);
+        console.log(list.toString());
+        arrayListSort.bubbleSort(list);
+        console.log(list.toString());
+        for (var i = 0; i < list.count - 1; ++i) {
+            expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
+        }
+    });
+
+    it('sort reversed', function() {
+        var list = new arrayList.ArrayList();
+        list.add(9);
+        list.add(8);
+        list.add(7);
+        list.add(6);
+        list.add(5);
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        list.add(0);
+        console.log(list.toString());
+        arrayListSort.bubbleSort(list);
+        console.log(list.toString());
+        for (var i = 0; i < list.count - 1; ++i) {
+            expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
+        }
+    });
+
+    it('sort sorted', function() {
+        var list = new arrayList.ArrayList();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        list.add(9);
+        console.log(list.toString());
+        arrayListSort.bubbleSort(list);
+        console.log(list.toString());
+        for (var i = 0; i < list.count - 1; ++i) {
+            expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
+        }
+    });
+});
