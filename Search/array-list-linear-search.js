13fd5dc90831c895124fb0efdc644c57ba6fa674
frebrezo <frebrezo@gmail.com>
2019-08-02 06:03:17 -0700

    Extracted arrayListLinearSearch function from addArrayListLinearSearch function.

--- Search/array-list-linear-search.js
+++ Search/array-list-linear-search.js
@@ -2,8 +2,17 @@
 
 const arrayList = require('../List/array-list');
 
+function arrayListLinearSearch(value) {
+    for (var i = 0; i < this.array.length; ++i) {
+        if (this.array[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 /**
- * Adds search function to ArrayList dynamically.
+ * Add search function to ArrayList dynamically.
  * https://stackoverflow.com/questions/3710275/does-javascript-have-the-interface-type-such-as-javas-interface
  * https://en.wikipedia.org/wiki/Duck_typing
  * @param {*} list 
@@ -16,14 +25,8 @@ function addArrayListLinearSearch(list) {
         return;
     }
     Object.defineProperty(list, 'search', {
-        value: function(value) {
-            for (var i = 0; i < this.array.length; ++i) {
-                if (this.array[i] === value) {
-                    return i;
-                }
-            }
-            return -1;
-        }
+        writable: true,
+        value: arrayListLinearSearch
     });
 }
 
