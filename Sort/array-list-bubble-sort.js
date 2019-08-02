c27d2543c10927894fce77339ecece20536d970c
frebrezo <frebrezo@gmail.com>
2019-08-02 04:26:58 -0700

    Refactored array list bubble sort method to Duck Type sort method.

--- Sort/array-list-bubble-sort.js
+++ Sort/array-list-bubble-sort.js
@@ -2,24 +2,34 @@
 
 var arrayList = require('../List/array-list');
 
-function bubbleSort(list) {
-    var iLast = list.count - 1;
-    for (var i = 0; i < iLast; ++i) {
-        var swapOccurred = false;
-        var jLast = list.count - 1 - i;
-        for (var j = 0; j < jLast; ++j) {
-            var leftValue = list.getAt(j);
-            var rightValue = list.getAt(j+1);
-            if (leftValue > rightValue) {
-                list.replaceAt(j+1, leftValue);
-                list.replaceAt(j, rightValue);
-                swapOccurred = true;
+function addArrayListBubbleSort(list) {
+    if (list.constructor !== arrayList.ArrayList) {
+        throw `list must be ArrayList [${list.constructor}].`;
+    }
+    if (typeof(list.sort) === 'function') {
+        return;
+    }
+    Object.defineProperty(list, 'sort', {
+        value: function() {
+            var iLast = this.array.length - 1;
+            for (var i = 0; i < iLast; ++i) {
+                var swapOccurred = false;
+                var jLast = this.array.length - 1 - i;
+                for (var j = 0; j < jLast; ++j) {
+                    var leftValue = this.array[j];
+                    var rightValue = this.array[j+1];
+                    if (leftValue > rightValue) {
+                        this.array[j+1] = leftValue;
+                        this.array[j] = rightValue;
+                        swapOccurred = true;
+                    }
+                }
+                if (!swapOccurred) {
+                    break;
+                }
             }
         }
-        if (!swapOccurred) {
-            break;
-        }
-    }
+    });
 }
 
-module.exports.bubbleSort = bubbleSort;
+module.exports.addArrayListBubbleSort = addArrayListBubbleSort;
--- Sort/array-list-bubble-sort.spec.js
+++ Sort/array-list-bubble-sort.spec.js
@@ -3,84 +3,116 @@
 var arrayList = require('../List/array-list');
 var arrayListSort = require('./array-list-bubble-sort');
 
-describe('array-list-sort', function() {
-    it('sort', function() {
-        var list = new arrayList.ArrayList();
-        list.add(8);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(1);
-        list.add(0);
-        list.add(9);
-        list.add(7);
-        list.add(2);
-        console.log(list.toString());
-        arrayListSort.bubbleSort(list);
-        console.log(list.toString());
-        for (var i = 0; i < list.count - 1; ++i) {
-            expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
-        }
+describe('array-list-bubble-sort', function() {
+    describe('addArrayListBubbleSort', function() {
+        it('search not exists', function() {
+            var list = new arrayList.ArrayList();
+            expect(typeof(list.sort)).toBe('undefined');
+        });
+
+        it('search exists', function() {
+            var list = new arrayList.ArrayList();
+            arrayListSort.addArrayListBubbleSort(list);
+            expect(typeof(list.sort)).toBe('function');
+        });
+
+        it('search already exists', function() {
+            var list = new arrayList.ArrayList();
+            arrayListSort.addArrayListBubbleSort(list);
+            arrayListSort.addArrayListBubbleSort(list);
+            arrayListSort.addArrayListBubbleSort(list);
+            expect(typeof(list.sort)).toBe('function');
+        });
+
+        it('object not ArrayList', function() {
+            var list = new Array(10);
+            expect(function() { arrayListSort.addArrayListBubbleSort(list); }).toThrow(`list must be ArrayList [function Array() { [native code] }].`);
+        });
     });
 
-    it('sort with duplicates', function() {
-        var list = new arrayList.ArrayList();
-        list.add(8);
-        list.add(1);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(1);
-        list.add(0);
-        list.add(9);
-        list.add(7);
-        list.add(1);
-        console.log(list.toString());
-        arrayListSort.bubbleSort(list);
-        console.log(list.toString());
-        for (var i = 0; i < list.count - 1; ++i) {
-            expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
-        }
-    });
-
-    it('sort reversed', function() {
-        var list = new arrayList.ArrayList();
-        list.add(9);
-        list.add(8);
-        list.add(7);
-        list.add(6);
-        list.add(5);
-        list.add(4);
-        list.add(3);
-        list.add(2);
-        list.add(1);
-        list.add(0);
-        console.log(list.toString());
-        arrayListSort.bubbleSort(list);
-        console.log(list.toString());
-        for (var i = 0; i < list.count - 1; ++i) {
-            expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
-        }
-    });
-
-    it('sort sorted', function() {
-        var list = new arrayList.ArrayList();
-        list.add(0);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        list.add(8);
-        list.add(9);
-        console.log(list.toString());
-        arrayListSort.bubbleSort(list);
-        console.log(list.toString());
-        for (var i = 0; i < list.count - 1; ++i) {
-            expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
-        }
+    describe('sort', function() {
+        it('sort', function() {
+            var list = new arrayList.ArrayList();
+            list.add(8);
+            list.add(3);
+            list.add(4);
+            list.add(5);
+            list.add(6);
+            list.add(1);
+            list.add(0);
+            list.add(9);
+            list.add(7);
+            list.add(2);
+            console.log(list.toString());
+            arrayListSort.addArrayListBubbleSort(list);
+            list.sort();
+            console.log(list.toString());
+            for (var i = 0; i < list.count - 1; ++i) {
+                expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
+            }
+        });
+    
+        it('sort with duplicates', function() {
+            var list = new arrayList.ArrayList();
+            list.add(8);
+            list.add(1);
+            list.add(4);
+            list.add(5);
+            list.add(6);
+            list.add(1);
+            list.add(0);
+            list.add(9);
+            list.add(7);
+            list.add(1);
+            console.log(list.toString());
+            arrayListSort.addArrayListBubbleSort(list);
+            list.sort();
+            console.log(list.toString());
+            for (var i = 0; i < list.count - 1; ++i) {
+                expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
+            }
+        });
+    
+        it('sort reversed', function() {
+            var list = new arrayList.ArrayList();
+            list.add(9);
+            list.add(8);
+            list.add(7);
+            list.add(6);
+            list.add(5);
+            list.add(4);
+            list.add(3);
+            list.add(2);
+            list.add(1);
+            list.add(0);
+            console.log(list.toString());
+            arrayListSort.addArrayListBubbleSort(list);
+            list.sort();
+            console.log(list.toString());
+            for (var i = 0; i < list.count - 1; ++i) {
+                expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
+            }
+        });
+    
+        it('sort sorted', function() {
+            var list = new arrayList.ArrayList();
+            list.add(0);
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            list.add(4);
+            list.add(5);
+            list.add(6);
+            list.add(7);
+            list.add(8);
+            list.add(9);
+            console.log(list.toString());
+            arrayListSort.addArrayListBubbleSort(list);
+            list.sort();
+            console.log(list.toString());
+            for (var i = 0; i < list.count - 1; ++i) {
+                expect(list.getAt(i) <= list.getAt(i+1)).toBeTruthy();
+            }
+        });
     });
 });
