9f1ea21284b7e9f5a334f949288e4f674d19d7e9
frebrezo <frebrezo@gmail.com>
2019-08-02 06:30:03 -0700

    Extracted function arrayListBubbleSort from addArrayListBubbleSort.

--- Sort/array-list-bubble-sort.js
+++ Sort/array-list-bubble-sort.js
@@ -2,34 +2,38 @@
 
 var arrayList = require('../List/array-list');
 
+function arrayListBubbleSort() {
+    var iLast = this.array.length - 1;
+    for (var i = 0; i < iLast; ++i) {
+        var swapOccurred = false;
+        var jLast = this.array.length - 1 - i;
+        for (var j = 0; j < jLast; ++j) {
+            var leftValue = this.array[j];
+            var rightValue = this.array[j+1];
+            if (leftValue > rightValue) {
+                this.array[j+1] = leftValue;
+                this.array[j] = rightValue;
+                swapOccurred = true;
+            }
+        }
+        if (!swapOccurred) {
+            break;
+        }
+    }
+}
+
 function addArrayListBubbleSort(list) {
     if (list.constructor !== arrayList.ArrayList) {
         throw `list must be ArrayList [${list.constructor}].`;
     }
     if (typeof(list.sort) === 'function') {
-        return;
+        list.sort = arrayListBubbleSort;
+    } else {
+        Object.defineProperty(list, 'sort', {
+            writable: true,
+            value: arrayListBubbleSort
+        });
     }
-    Object.defineProperty(list, 'sort', {
-        value: function() {
-            var iLast = this.array.length - 1;
-            for (var i = 0; i < iLast; ++i) {
-                var swapOccurred = false;
-                var jLast = this.array.length - 1 - i;
-                for (var j = 0; j < jLast; ++j) {
-                    var leftValue = this.array[j];
-                    var rightValue = this.array[j+1];
-                    if (leftValue > rightValue) {
-                        this.array[j+1] = leftValue;
-                        this.array[j] = rightValue;
-                        swapOccurred = true;
-                    }
-                }
-                if (!swapOccurred) {
-                    break;
-                }
-            }
-        }
-    });
 }
 
 module.exports.addArrayListBubbleSort = addArrayListBubbleSort;
